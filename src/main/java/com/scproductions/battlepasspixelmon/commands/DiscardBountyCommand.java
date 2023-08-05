6d8a37d2e3aa595d2f6818d9ea4abeee8f96f79d
Stephen <srcjemail665@gmail.com>
2023-08-05 15:37:21 -0500

    Update DiscardBountyCommand.java
    Add DiscardBounty functionality.

--- src/main/java/com/scproductions/battlepasspixelmon/commands/DiscardBountyCommand.java
+++ src/main/java/com/scproductions/battlepasspixelmon/commands/DiscardBountyCommand.java
@@ -1,5 +1,33 @@
 package com.scproductions.battlepasspixelmon.commands;
 
-public class DiscardBountyCommand {
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.scproductions.battlepasspixelmon.bounties.BountyManager;
+import com.scproductions.battlepasspixelmon.bounties.BountyManager.Bounty;
 
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
+
+public class DiscardBountyCommand {
+	public DiscardBountyCommand(CommandDispatcher<CommandSource> dispatcher) {
+		dispatcher.register(
+				Commands.literal("discardbounty")
+						.then(Commands.argument("number", IntegerArgumentType.integer(1))
+							.then(Commands.literal("--taskprogresswillbereset"))
+								.executes((command) -> {return discardBounty(command.getSource(), IntegerArgumentType.getInteger(command, "number"));})));
+	}
+	
+	private int discardBounty(CommandSource source, int number) throws CommandSyntaxException {
+		
+		ServerPlayerEntity player = source.getPlayerOrException();
+		Bounty bounty = BountyManager.getPlayerAcceptedBounties(player.getUUID()).get(number);
+		BountyManager.discardBounty(player.getUUID(), bounty.uuid);
+		player.sendMessage(new StringTextComponent("Discarded the following bounty: " + bounty.getFormattedString() + " You can pick it back up if it's still active, with reset progress."), player.getUUID());
+		
+		
+		return 1;
+	}
 }
