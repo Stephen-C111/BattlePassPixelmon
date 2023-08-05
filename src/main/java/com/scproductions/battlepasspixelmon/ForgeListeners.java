3e42c5d7eb193ae75592c52961254d5a2e985d64
Stephen <srcjemail665@gmail.com>
2023-08-05 17:32:54 -0500

    Add Listener for Ore Harvest
    When a valuable block that drops exp is broken, grant pass progress.

--- src/main/java/com/scproductions/battlepasspixelmon/ForgeListeners.java
+++ src/main/java/com/scproductions/battlepasspixelmon/ForgeListeners.java
@@ -7,6 +7,7 @@ import com.scproductions.battlepasspixelmon.commands.AcceptBountyCommand;
 import com.scproductions.battlepasspixelmon.commands.CheckRankCommand;
 import com.scproductions.battlepasspixelmon.commands.ClaimRewardPacksCommand;
 import com.scproductions.battlepasspixelmon.commands.ClaimRewardsCommand;
+import com.scproductions.battlepasspixelmon.commands.DiscardBountyCommand;
 import com.scproductions.battlepasspixelmon.commands.GetJournalCommand;
 import com.scproductions.battlepasspixelmon.commands.GiveProgressCommand;
 import com.scproductions.battlepasspixelmon.commands.GiveSelfProgressCommand;
@@ -18,11 +19,14 @@ import com.scproductions.battlepasspixelmon.commands.ToggleBattlePassBossBarComm
 import com.scproductions.battlepasspixelmon.commands.ToggleBattlePassMessagesCommand;
 import com.scproductions.battlepasspixelmon.commands.ViewAcceptedBountiesCommand;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.brewing.PlayerBrewedPotionEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
@@ -47,6 +51,7 @@ public class ForgeListeners {
 		new GetJournalCommand(event.getDispatcher());
 		new AcceptBountyCommand(event.getDispatcher());
 		new ViewAcceptedBountiesCommand(event.getDispatcher());
+		new DiscardBountyCommand(event.getDispatcher());
 		
 		ConfigCommand.register(event.getDispatcher());
 	}
@@ -82,6 +87,17 @@ public class ForgeListeners {
 			}
 			//BountyGUIHandler.bgui.openBountyBoard((ServerPlayerEntity) event.getEntity());
 		}
+	}
+	
+	@SubscribeEvent
+	public static void onPlayerHarvest(BlockEvent.BreakEvent event) {
+		PlayerEntity player = event.getPlayer();
+		int xp = event.getExpToDrop();
+		if (xp > 0) {
+			BattlePassManager.grantProgress(player.getUUID(), 1, false);
+			BattlePassManager.sendPlayerInfo((ServerPlayerEntity) player, 1, "Mined a valuable block:");
+		}
+		
 	}
 	
 }
